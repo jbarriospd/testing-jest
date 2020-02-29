651de89b64854d50c94dd70963b6c7d2dcfae468
jbarriospd <jbarriospd@MacBook-Pro-de-jose.local>
2020-02-29 10:28:22 -0500

    testing ready for deploy

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import {create} from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -14,3 +15,10 @@ describe('<Footer />', () => {
     });
 
 });
+
+describe('Footer Snapshot', () => {
+    test('Comprobar la UI del componente Footer', () => {
+      const footer = create(<Footer />);
+      expect(footer.toJSON()).toMatchSnapshot();
+    });
+  });
